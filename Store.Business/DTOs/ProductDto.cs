5ed19bf64dfb4c7ef5fa0b5d576ed24910d6957e
Samir Mejia <samir04m.dev@gmail.com>
2025-02-23 22:27:24 -0500

    Se agregan JSON Attributes al modelo ProductDto

--- Store.Business/DTOs/ProductDto.cs
+++ Store.Business/DTOs/ProductDto.cs
@@ -1,12 +1,33 @@
-﻿namespace Store.Business.DTOs
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Store.Business.DTOs
 {
     public class ProductDto
     {
+        [Key]
+        [JsonProperty("id")]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [JsonProperty("name")]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        [JsonProperty("description")]
         public string Description { get; set; }
+
+        [StringLength(500)]
+        [JsonProperty("image_url")]
         public string ImageUrl { get; set; }
+
+        [Required]
+        [JsonProperty("category_id")]
         public int CategoryId { get; set; }
+
+        [JsonProperty("category_name")]
         public string CategoryName { get; set; }
     }
 }
