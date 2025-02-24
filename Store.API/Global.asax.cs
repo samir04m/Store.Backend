717b233d179d0f611d306ae92dfff5815ae2c78c
Samir Mejia <samir04m.dev@gmail.com>
2025-02-23 22:04:54 -0500

    Se quitan archivos innecesarios

--- Store.API/Global.asax.cs
+++ Store.API/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using Store.API.Startup;
+using System;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -19,6 +20,14 @@ namespace Store.API
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (Request.Url.AbsolutePath == "/")
+            {
+                Response.Redirect("~/Help");
+            }
         }
     }
 }
